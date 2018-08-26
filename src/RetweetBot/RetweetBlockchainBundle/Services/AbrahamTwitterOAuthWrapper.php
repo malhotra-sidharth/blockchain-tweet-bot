343c0218c8b5a7151cf83f05d8656fc46644ef7f
sidharth <sidharthmalhotra0094@gmail.com>
2018-08-26 13:51:28 -0400

    [WIP] Added api for retweet and getting tweets for #blockchain in AbrahamTwitterOAuthWrapper

--- src/RetweetBot/RetweetBlockchainBundle/Services/AbrahamTwitterOAuthWrapper.php
+++ src/RetweetBot/RetweetBlockchainBundle/Services/AbrahamTwitterOAuthWrapper.php
@@ -24,10 +24,45 @@ class AbrahamTwitterOAuthWrapper {
 
   /**
    * Gets the connection for the Twitter API
-   * 
+   *
    * @return TwitterOAuth
    */
   public function getConnection(): TwitterOAuth {
     return $this->twitterOAuth;
+  }
+
+  /**
+   * Verifies the the token and secret credentials for the user
+   *
+   * @return object
+   */
+  public function verifyDetails(): object {
+    return $this->twitterOAuth->get("account/verify_credentials");
+  }
+
+  /**
+   * Gets most recent 50 Tweets for #blockchain in language english
+   *
+   * @return array
+   */
+  public function getTweetsForBlockchainTag(): array {
+    $tweets = $this->twitterOAuth->get("search/tweets",
+        [
+            "q"    => "#blockchain",
+            "lang" => "en",
+            "count" => 50
+        ]);
+
+    return $tweets['statuses'];
+  }
+
+  /**
+   * Posts a retweet with the given id
+   *
+   * @param int $id
+   * Id of the tweet to be retweeted
+   */
+  public function retweet(int $id) {
+    $this->twitterOAuth->post("statuses/retweet", ["id" => $id]);
   }
 }
