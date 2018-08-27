4cf36300219dc2fe3c861bdfe8b678265208e4d1
sidharth <sidharthmalhotra0094@gmail.com>
2018-08-26 21:43:18 -0400

    [TASK] Added support to get tweets, filter and retweet them and add to the cache

--- src/RetweetBot/RetweetBlockchainBundle/Controller/BlockchainController.php
+++ src/RetweetBot/RetweetBlockchainBundle/Controller/BlockchainController.php
@@ -2,21 +2,47 @@
 
 namespace RetweetBot\RetweetBlockchainBundle\Controller;
 
+use RetweetBot\RetweetBlockchainBundle\Entities\Tweet;
+use RetweetBot\RetweetBlockchainBundle\Services\AbrahamTwitterOAuthWrapper;
+use RetweetBot\RetweetBlockchainBundle\Services\RedisWrapper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class BlockchainController extends Controller
-{
+class BlockchainController extends Controller {
 
-    /**
-     * Fetches 100 tweets with hashtag #blockchain and finds top 10 tweets
-     * that hasn't been retweeted. Makes sure that only one tweet from a user 
-     * is retweeted in a day
-     */
-    public function fetchAndCalculateAction() {
-        $consumer_secret = $this->getParameter('consumer_secret');
-        $consumer_key = $this->getParameter('consumer_key');
-        $access_token = $this->getParameter('access_token');
-        $access_secret = $this->getParameter('access_secret');
+  /**
+   * Fetches 100 tweets with hashtag #blockchain and finds top 10 tweets
+   * that hasn't been retweeted. Makes sure that only one tweet from a user
+   * is retweeted in a day
+   */
+  public function fetchAndCalculateAction() {
+    /** @var AbrahamTwitterOAuthWrapper $twitterOAuth */
+    $twitterOAuth = $this->container->get('fetch.tweets.abraham.twitteroauth');
 
-    }
+    /** @var RedisWrapper $cache */
+    $cache = $this->container->get('retweetbot.redis');
+
+//    $tweets = $twitterOAuth->getTweetsForBlockchainTag();
+
+    $retweets = [];
+
+//    foreach ($tweets as $tweet) {
+//      // create tweet object
+//      $tweetObj = new Tweet($tweet);
+//
+//      // check if the user's limit for today has been reached or not
+//      if (!$cache->hasItem($tweetObj->getUser()->getId())) {
+//        $retweets[] = $tweetObj->getId();
+//        $cache->putItem($tweetObj->getUser()->getId());
+//      }
+//    }
+//
+//    // if not then retweet else do nothing
+//    foreach ($retweets as $retweet) {
+//      $twitterOAuth->retweet($retweet);
+//    }
+
+    $result = $cache->hasItem(763780322756587520);
+    return new JsonResponse(["status" => $result]);
+  }
 }
